0e54ec8cea9e8e3529b7b3462b15e05bfeb2cf82
e3_schmidt <user@gibts.net>
2014-06-11 09:29:35 +0200

    los

---
+++ src/BildBauer.java
@@ -0,0 +1,18 @@
+import javax.swing.ImageIcon;
+import java.lang.String;
+import java.lang.System;
+
+public class BildBauer {
+
+    public ImageIcon createImageIcon(String pfad) {
+        java.net.URL imgURL = getClass().getResource(pfad);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, "");
+        }
+        else {
+            System.out.println("Konnte " + pfad + " nicht finden.");
+            return null;
+        }
+    }
+
+}
---
+++ src/MeinLabel.java
@@ -0,0 +1,24 @@
+import javax.swing.*;import javax.swing.Icon;import javax.swing.JLabel;import javax.swing.SwingConstants;
+import java.awt.*;import java.awt.Color;import java.lang.String;
+
+public class MeinLabel extends JLabel {
+
+    public MeinLabel(String name, boolean opaque, Color color) {
+
+        setText(name);
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setOpaque(opaque);
+        setBackground(color);
+
+    }
+
+    public MeinLabel(Icon icon, boolean opaque, Color color) {
+
+        setIcon(icon);
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setOpaque(opaque);
+        setBackground(color);
+
+    }
+
+}
